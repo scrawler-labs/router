51d78cf4ab2087c4b6d53be786aee48fdff2f6f2
Pranjal Pandey <pranjaltech10@gmail.com>
2021-10-07 09:37:45 +0530

    Update RouterTest.php

--- Tests/RouterTest.php
+++ Tests/RouterTest.php
@@ -6,7 +6,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 class RouterTest extends TestCase
 {
-    private $request;
+    private $hello_request;
+    private $bye_request;
     private $router;
     function __construct(){
     parent::__construct();
@@ -15,10 +16,16 @@ class RouterTest extends TestCase
     $namespace = "Tests\Demo";
 
     $collection = new \Scrawler\Router\RouteCollection($dir,$namespace);
-    $this->request = Request::create(
+    $this->hello_request = Request::create(
     '/hello/world/pranjal',
     'GET'
      );
+       
+    $this->blank_request = Request::create(
+    '/bye/world/nobody',
+    'GET'
+     );
+   
 
     $this->router = new \Scrawler\Router\Router($collection,$this->request);
     }
@@ -29,6 +36,8 @@ class RouterTest extends TestCase
     function testDispatch(){
       $response = $this->router->dispatch();
       $this->assertEquals('Hello pranjal',$response->getContent());
+      $this->assertEquals('Bye nobody',$response->getContent());
+
     }
 
 
